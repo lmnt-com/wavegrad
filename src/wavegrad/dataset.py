Fix audio normalization
Newer versions of TorchAudio return normalized PCM samples instead
of unnormalized PCM samples as before. This change bumps up the
minimum required version of TorchAudio and assumes it will normalize
the signal for us.

Issue: #11

src/wavegrad/dataset.py
@@ -36,10 +36,10 @@ class NumpyDataset(torch.utils.data.Dataset):
   def __getitem__(self, idx):
     audio_filename = self.filenames[idx]
     spec_filename = f'{audio_filename}.spec.npy'
-    signal, _ = torchaudio.load_wav(audio_filename)
+    signal, _ = torchaudio.load(audio_filename)
     spectrogram = np.load(spec_filename)
     return {
-        'audio': signal[0] / 32767.5,
+        'audio': signal[0],
         'spectrogram': spectrogram.T
     }
 

src/wavegrad/noise_schedule.py
@@ -64,8 +64,8 @@ def main(args):
   max_audio_len = 0
   max_spec_len = 0
   for filename in audio_filenames[:args.batch_size]:
-    clip, _ = T.load_wav(filename)
-    clip = clip[0].numpy() / 32767.5
+    clip, _ = T.load(filename)
+    clip = clip[0].numpy()
     spec = np.load(f'{filename}.spec.npy')
     audio.append(clip)
     spectrogram.append(spec)

src/wavegrad/preprocess.py
@@ -27,10 +27,10 @@ from wavegrad.params import params
 
 
 def transform(filename):
-  audio, sr = T.load_wav(filename)
+  audio, sr = T.load(filename)
   if params.sample_rate != sr:
     raise ValueError(f'Invalid sample rate {sr}.')
-  audio = torch.clamp(audio[0] / 32767.5, -1.0, 1.0)
+  audio = torch.clamp(audio[0], -1.0, 1.0)
 
   hop = params.hop_samples
   win = hop * 4